da6d40ee08f11acdd22754dccade8e01acc3f546
Toon-ABCTech <suphareuk@gmail.com>
2011-10-26 09:44:58 +0700

    fizz buzz codeKata practice

---
+++ fizzBuzz.js
@@ -0,0 +1,12 @@
+﻿function fizzBuzz(num) {
+    if (num % 3 === 0 && num % 5 === 0) {
+        num = "fizz buzz";
+    } else if (num % 3 === 0) {
+        num = "fizz";
+    } else if (num % 5 === 0) { 
+        num = "buzz";
+    } 
+    return num;
+}
+
+console.log(fizzBuzz(num));
