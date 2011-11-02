e3f0068d58e90972cc5cc15443e6bb54edf85d17
Toon-ABCTech <suphareuk@gmail.com>
2011-11-02 17:15:05 +0700

    improve fizz buzz 2 codes

--- fizzBuzz2.js
+++ fizzBuzz2.js
@@ -2,28 +2,22 @@ var returnValue = [],
 	returnNum = 0;
 	returnFizz = " Fizz",
 	returnBuzz = " Buzz",
-	returnFizzBuzz = " Fizz Buzz";	
+	returnFizzBuzz = " Fizz Buzz",	
+	num = [];
 				
-function fizzBuzz() {
-	for (var i=1; i < 100; i++) {
-		if (i !== 0) {
-			returnValue[i] = chkFizzBuzz(i);
-		}
+function fizzBuzz(param){
+	for (var i=0; i < param; i++) {
+		if(i % 3 === 0 && i % 5 === 0 && i != 0){
+			num[i] = returnFizzBuzz;
+		}else if(i % 3 === 0 && i != 0){
+			num[i] = returnFizz;
+		}else if(i % 5 === 0 && i != 0){
+			num[i] = returnBuzz;
+		}else {
+			num[i] = i;
+		}		
 	}
-	return returnValue;
+	return num;
 }
 
-function chkFizzBuzz(num){
-	if(num % 3 === 0 && num % 5 === 0){
-		return returnFizzBuzz;
-	}else if(num % 3 === 0){
-		return returnFizz;
-	}else if(num % 5 === 0){
-		return returnBuzz;
-	}else {
-		returnNum = num;
-		return returnNum;
-	}
-}
-
-console.log(fizzBuzz());
+console.log(fizzBuzz(100));
